e0497833086a8d6066e1af8a1804e08ccb44ccb5
Jaqobel <me.jakub.gil@gmail.com>
2023-07-12 22:37:00 +0200

    Saving usefull code

--- main.rb
+++ main.rb
@@ -1,1 +1,19 @@
-print "Hello World\n"
+# puts("Me: \"Jakub\"  \\") # How to use "  and \ in print
+
+# phrase = "My name is"
+
+# puts("Jakub         ".strip())
+# puts("Jakub         ".strip().length())
+
+# puts phrase.include? "is" # Important
+
+# puts phrase[0,4]
+
+# puts phrase.index("name") # Where string "name" starts
+
+# puts 2**3
+# puts 10%3
+
+puts "Enter a number between 1 and 10"
+number = gets.chomp()
+puts ("Your number is " + number + ", Thank You!")
